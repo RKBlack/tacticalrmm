5f9bfe1e898961bc94fcc0e3a354260fa8d942f8
jbloomfield80 <75686387+jbloomfield80@users.noreply.github.com>
2024-10-03 13:40:27 -0500

    Update views.py
    Added pagination to /beta/v1/client/views/py

--- api/tacticalrmm/beta/v1/client/views.py
+++ api/tacticalrmm/beta/v1/client/views.py
@@ -3,11 +3,13 @@ from rest_framework.permissions import IsAuthenticated
 
 from clients.models import Client
 from clients.permissions import ClientsPerms
+from beta.v1.pagination import StandardResultsSetPagination
 from ..serializers import ClientSerializer
 
 
 class ClientViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated, ClientsPerms]
     queryset = Client.objects.all()
+    pagination_class = StandardResultsSetPagination
     serializer_class = ClientSerializer
     http_method_names = ["get", "put"]
